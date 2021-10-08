5132b53fe0347f204d2acd608c2fcf300870e86f
rnintai <ho46825@gmail.com>
2021-10-08 17:50:11 +0900

    Feat: mariaDB 연결 테스트 코드

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+// mariadb connection
+const maria = require('./maria');
+maria.connect();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
