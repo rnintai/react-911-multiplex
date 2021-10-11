1ac0b8e8891f2ebb4e3380edcbeed41720071953
rnintai <ho46825@gmail.com>
2021-10-11 21:22:19 +0900

    Fix: maria -> mysql

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-const maria = require('../mysql');
+const mysql = require('../mysql');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  maria.query('select * from test', function(err, rows, fields) { // 쿼리문을 이용해 데이터를 가져온다.
+  mysql.query('select * from test', function(err, rows, fields) { // 쿼리문을 이용해 데이터를 가져온다.
     if(!err) { // 에러가 없다면
       res.send(rows); // rows 를 보내주자
     } else { // 에러가 있다면?
