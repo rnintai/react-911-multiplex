90a58a6aa47895ceceedf8c8923a487a14149c1c
rnintai <ho46825@gmail.com>
2021-10-14 16:15:45 +0900

    chore: prettier, eslint 설치, 불필요한 파일 삭제

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const signUpRouter = require('./routes/signUp');
 
 var app = express();
-// mariadb connection
+
+// mysql connection
 const mysql = require('./mysql');
-mysql.connect(function(err){
+mysql.connect(function (err) {
   if (err) throw err;
   console.log('mysql connected.');
 });
@@ -24,18 +25,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // routers
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/signUp', signUpRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- backend/mysql.js
+++ backend/mysql.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 const mysql = require('mysql');
 const conn = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database:'multiplex'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: 'multiplex',
 });
 
 module.exports = conn;
