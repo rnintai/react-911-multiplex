a6eec83f01da569e24a625455eec4123ac1da56a
rnintai <ho46825@gmail.com>
2021-10-29 01:52:50 +0900

    comment: 주석 작업

--- backend/routes/movies/fetchMoviesDetail.js
+++ backend/routes/movies/fetchMoviesDetail.js
@@ -12,25 +12,13 @@ router.get("/", async function (req, res) {
     updatedCount: 0,
   };
 
-  //   const a = await mysql.query(
-  //     "SELECT movie_id FROM movie",
-  //     function (err, result) {
-  //       if (!err) {
-  //         for (const item of result) {
-  //           movieIdList.push(item.movie_id);
-  //         }
-  //       } else {
-  //         res.send(err);
-  //       }
-  //     }
-  //   );
   let rows = await mysql.query("SELECT movie_id FROM movie");
   let movieIdList = rows[0];
-  // 모든 페이지 iterate하며 db에 추가.
+  // 데이터베이스 내 모든 레코드를 iterate하며 db에 추가.
   try {
     for (const element of movieIdList) {
       let movieId = element.movie_id;
-
+      // kobis API에서 현재 영화의
       let response = await axios
         .get(
           `http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=${process.env.KOBIS_KEY}&movieCd=${movieId}`
@@ -42,6 +30,7 @@ router.get("/", async function (req, res) {
 
       let showTm = movieDetail.showTm;
 
+      // 배우1(배역1),배우2(배역2),... 형태의 문자열로 변형
       let actorsList = [];
       for (let actor of movieDetail.actors) {
         actorsList.push(
@@ -53,12 +42,14 @@ router.get("/", async function (req, res) {
       let actors = actorsList.join(",");
 
       let audits = movieDetail.audits[0].watchGradeNm;
+      // 상영 형태1,상영 형태2,... 의 문자열로 변형
       let showTypesList = [];
       for (const showType of movieDetail.showTypes) {
         showTypesList.push(showType.showTypeGroupNm);
       }
       let showTypes = showTypesList.join(",");
 
+      // update 쿼리문
       const sql = `UPDATE movie SET running_time=?, actors=?, age_limit=?, show_type=? WHERE movie_id=${movieId}`;
       let insertResult = await mysql.query(sql, [
         showTm,
@@ -67,6 +58,7 @@ router.get("/", async function (req, res) {
         showTypes,
       ]);
       console.log(insertResult[0]);
+      // 변경 사항이 있을 경우 그 개수를 카운팅하여 결과로 돌려줌.
       if (insertResult[0].changedRows != 0) {
         responseObj.updatedCount += 1;
       }
