fd7275511498c68328f49e978d8d00b65be4fa04
rnintai <ho46825@gmail.com>
2021-10-29 00:16:09 +0900

    chore: 비동기 처리를 위한 mysql2 세팅

--- backend/mysql.js
+++ backend/mysql.js
@@ -1,12 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const mysql = require('mysql');
+const mysql = require("mysql2");
+
 const conn = mysql.createConnection({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: 'multiplex',
+  database: "multiplex",
 });
 
-module.exports = conn;
+const promiseConn = conn.promise();
+
+module.exports = promiseConn;
