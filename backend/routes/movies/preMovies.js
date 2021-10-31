68e702e6830a39ef7dd548fbd787a3529e591a4d
rnintai <ho46825@gmail.com>
2021-10-31 14:55:08 +0900

    Feat: [preMovies] 개봉예정 영화 조회 API 구현

--- backend/routes/movies/preMovies.js
+++ backend/routes/movies/preMovies.js
@@ -1,29 +1,36 @@
-const axios = require("axios");
 const express = require("express");
 const router = express.Router();
 require("dotenv").config();
 
-// const mysql = require('../mysql');
+const pool = require("../../mysql");
 
-/* GET boxoffice from kobis. */
+/**
+ * /omvies/pre?page=조회 페이지
+ * 박스오피스에서 개봉한 영화 중 예매율 상위 10위. 예매율이 같을 경우 이름순으로 정렬.
+ * 추후
+ */
 router.get("/", async function (req, res) {
-  // 페이지로 나누어 조회
+  let connection = await pool.getConnection((conn) => conn);
+
+  const itemsPerPage = 12;
+  const startIndex = req.query.page * itemsPerPage;
   try {
-    const response = await axios
-      .get(
-        `http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=${process.env.KOBIS_KEY}&curPage=${req.query.page}&itemPerPage=10&openStartDt=2021&openEndDt=2021&movieTypeCd=220101&repNationCd=22041011`
-      )
-      .catch((error) => {
-        res.status(400).send(error);
-      });
-    let movieList = response.data.movieListResult.movieList;
-    let newList = movieList.filter((element) => {
-      return element.prdtStatNm == "개봉예정";
-    });
-    console.log(movieList);
-    res.status(200).send(newList);
-  } catch (error) {
-    console.log(error);
+    const sql = `SELECT * FROM movie
+      WHERE movie_state="개봉예정"
+      ORDER BY released_at ASC
+      LIMIT ${startIndex}, ${itemsPerPage}`;
+
+    const result = await connection.query(sql);
+
+    connection.release();
+
+    console.log("success");
+    res.status(200).send(result[0]);
+  } catch (err) {
+    connection.release();
+
+    console.log(err);
+    res.status(400).send(err);
   }
 });
 
