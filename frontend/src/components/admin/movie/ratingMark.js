ae4e2832af113fc760fe13d0e040bbb280d030f1
rnintai <ho46825@gmail.com>
2021-11-30 19:11:17 +0900

    Feat: 관람등급 컴포넌트 구현

--- frontend/src/components/admin/movie/ratingMark.js
+++ frontend/src/components/admin/movie/ratingMark.js
@@ -1,12 +1,17 @@
 import React from "react";
+import "./ratingmark.scss";
 
 const RatingMark = ({ rate }) => {
   return (
     <>
-      {rate === "전체관람가" && <div>전체</div>}
-      {rate === "12세이상관람가" && <div>12</div>}
-      {rate === "15세이상관람가" && <div>15</div>}
-      {rate === "청소년관람불가" && <div>19</div>}
+      {rate === "전체관람가" && <div className="rating-mark green">전체</div>}
+      {rate === "12세이상관람가" && (
+        <div className="rating-mark blue">12세</div>
+      )}
+      {rate === "15세이상관람가" && (
+        <div className="rating-mark orange">15세</div>
+      )}
+      {rate === "청소년관람불가" && <div className="rating-mark red">청불</div>}
     </>
   );
 };
