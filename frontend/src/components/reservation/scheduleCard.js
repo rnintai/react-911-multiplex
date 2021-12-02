591ec221f9ac41c9edfbd50c86b8bc256367ea1c
rnintai <ho46825@gmail.com>
2021-12-02 20:56:52 +0900

    Design: 날짜 선택 스타일 수정

--- frontend/src/components/reservation/scheduleCard.js
+++ frontend/src/components/reservation/scheduleCard.js
@@ -29,26 +29,33 @@ function ScheduleCard({
         {/* <Font boldness={FontBold.bold} style={{ marginBottom: 5 }}>
           {date}
         </Font> */}
-        <Font
-          size={FontSize.sm}
-          boldness={FontBold.bold}
-          color={FontColor.gray75}
-        >
-          {startTime}~{endTime}
+        {/* <div> */}
+        <Font color={FontColor.black}>{startTime}</Font>
+        <Font size={FontSize.xs} color={FontColor.gray75}>
+          ~{endTime}
         </Font>
+        {/* </div> */}
       </div>
       <div className="flex-col" style={{ width: "60%" }}>
         <Font className="col-1_1">{movieName}</Font>
+        <Font
+          size={FontSize.xs}
+          boldness={FontBold.bold}
+          color={FontColor.gray75}
+          className="col-1_1"
+        >
+          {theaterType}
+        </Font>
       </div>
       <div className="flex-col" style={{ width: "15%" }}>
         <Font
-          size={FontSize.sm}
+          size={FontSize.xs}
           className="text-right"
           style={{ marginBottom: 5 }}
         >
-          {theaterName} [{theaterType}]
+          {theaterName}
         </Font>
-        <Font size={FontSize.sm} className="text-right">
+        <Font size={FontSize.xs} className="text-right">
           {availSeat}/{totalSeat}
         </Font>
       </div>
