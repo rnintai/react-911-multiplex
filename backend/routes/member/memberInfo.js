19c23e9051e71a0e20c0ab61c2a7e98a6d6e13c4
rnintai <ho46825@gmail.com>
2021-12-07 12:36:51 +0900

    Feat: 유저정보 조회 API admin여부 반환

--- backend/routes/member/memberInfo.js
+++ backend/routes/member/memberInfo.js
@@ -3,10 +3,22 @@ const router = express.Router();
 
 const pool = require("../../mysql");
 
+// /member/info/:memberID
 router.get("/:memberID", async function (req, res) {
   let conn = await pool.getConnection();
 
-  const sql = `SELECT member_id, name, email, birthday, phone_number, gender, signin_date, address from member WHERE member_id='${req.params.memberID}'`;
+  const sql = `
+  SELECT member_id, 
+  name, 
+  email, 
+  birthday, 
+  phone_number, 
+  gender, 
+  signin_date, 
+  address,
+  isAdmin
+  from member 
+  WHERE member_id='${req.params.memberID}'`;
 
   try {
     const response = await conn.query(sql);
