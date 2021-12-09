0e50f3df33f48dd17b5c263f60c35c8182c38ac5
rnintai <ho46825@gmail.com>
2021-12-09 17:58:59 +0900

    Design: 영화 상세정보 디자인 수정

--- frontend/src/components/movie_info/Detail.js
+++ frontend/src/components/movie_info/Detail.js
@@ -3,6 +3,12 @@
 import React, { Component } from "react";
 import "./Movie_info.css";
 import axios from "axios";
+import {
+  Font,
+  FontBold,
+  FontColor,
+  FontSize,
+} from "src/design-system/font/Font";
 
 const API =
   window.location.hostname === "localhost" ? "http://localhost:5000" : "/api";
@@ -31,20 +37,45 @@ class Movie_info extends Component {
 
   render() {
     return (
-      <div className="Detail_box">
-        <section>
-          <img className="Movie_img" src={this.state.MovieInfo.poster} />
-          <p className="Movie_name"> {this.state.MovieInfo.movie_name} </p>
-          <p className="Info">
-            장르 : {this.state.MovieInfo.genre} 개봉 :{" "}
-            {this.state.MovieInfo.released_at}러닝타임 :{" "}
-            {this.state.MovieInfo.running_time}분
-          </p>
-          <p className="Info">감독: {this.state.MovieInfo.director}</p>
-          <p className="Info">출연진 : {this.state.MovieInfo.actors}</p>
-        </section>
-        <h2>영화 정보</h2>
-        <p>{this.state.MovieInfo.synopsis}</p>
+      <div className="Detail_box flex-col">
+        <img
+          className="Movie_img"
+          src={this.state.MovieInfo.poster}
+          style={{ marginBottom: 10 }}
+        />
+        <Font
+          size={FontSize.xl}
+          boldness={FontBold.bold}
+          className="Movie_name"
+        >
+          {this.state.MovieInfo.movie_name}{" "}
+        </Font>
+        <div className="Info flex-row">
+          <Font style={{ marginRight: 10 }}>
+            장르 : {this.state.MovieInfo.genre}
+          </Font>
+          <Font style={{ marginRight: 10 }}>
+            개봉 :{" "}
+            {this.state.MovieInfo.released_at &&
+              this.state.MovieInfo.released_at.split("T")[0]}
+          </Font>
+          <Font style={{ marginRight: 10 }}>
+            러닝타임 : {this.state.MovieInfo.running_time}분
+          </Font>
+        </div>
+        <Font className="Info">감독: {this.state.MovieInfo.director}</Font>
+        <Font className="Info">출연진 : {this.state.MovieInfo.actors}</Font>
+        <div className="flex-col">
+          <Font
+            size={FontSize.xl}
+            boldness={FontBold.bold}
+            className="Movie_name"
+            style={{ marginBottom: 8 }}
+          >
+            영화 정보
+          </Font>
+          <Font>{this.state.MovieInfo.synopsis}</Font>
+        </div>
       </div>
     );
   }
